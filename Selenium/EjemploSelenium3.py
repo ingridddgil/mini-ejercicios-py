310184c4c0caee28c81ed37f33c7301bb9cfb4b5
ingridd <ingrid.sglh@gmail.com>
2024-12-05 11:33:20 -0600

    act

--- Selenium/EjemploSelenium3.py
+++ Selenium/EjemploSelenium3.py
@@ -3,15 +3,39 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 
 driver = webdriver.Chrome()
 driver.get("http://www.pino.mx/ti/lista.php")
-lista = []
+
+matriculas = []
+
 for i in range(1, 47):
     campo = f"m_{i}"
-    lista.append(WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.ID, campo))).get_dom_attribute("value"))
+    matriculas.append(WebDriverWait(driver, 8).until(EC.presence_of_element_located((By.ID, campo))).get_dom_attribute("value"))
 
-driver2 = driver.Chrome()
+sum = 1
+   
+for matricula in matriculas:
+    driver.get("http://www.pino.mx/ti/codigo01.php")
 
-driver2.get("http://www.pino.mx/ti/codigo01.php")
+    campo_matricula = WebDriverWait(driver, 7).until(EC.presence_of_element_located((By.ID, "matricula")))
+    campo_matricula.send_keys(matricula)
+    campo_matricula.send_keys(Keys.ENTER)
+
+    codigo = WebDriverWait(driver, 7).until(EC.presence_of_element_located((By.ID, "codigo"))).get_dom_attribute("value")
+
+    driver.get("http://www.pino.mx/ti/acceso01.php")
+
+    campo_matricula = WebDriverWait(driver, 7).until(EC.presence_of_element_located((By.ID, "matricula")))
+    campo_matricula.send_keys(matricula)
+
+    campo_codigo = WebDriverWait(driver, 7).until(EC.presence_of_element_located((By.ID, "codigo")))
+    campo_codigo.send_keys(codigo)
+    campo_codigo.send_keys(Keys.ENTER)
+
+    id = WebDriverWait(driver, 7).until(EC.presence_of_element_located((By.ID, "confirmacion"))).get_dom_attribute("value")
+
+    print(f"{sum} Matricula: {matricula}\nCodigo: {codigo}\nID de cofirmación: {id}")
+    sum += 1
+
+    
